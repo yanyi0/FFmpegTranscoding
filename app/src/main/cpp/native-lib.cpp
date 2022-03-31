c5b3a0458b9f35fb33a3ca864d83f6489dd8bfb1
yandongyun <yandongyun@zego.im>
2022-03-31 19:45:34 +0800

    integrate ffmpeg into project

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,45 @@
+#include <jni.h>
+#include <string>
+#include <unistd.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+
+JNIEXPORT jstring JNICALL
+Java_com_fish_ffmpegtranscoding_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+    }
+JNIEXPORT jstring JNICALL
+Java_com_fish_ffmpegtranscoding_MainActivity_ffmpegInfo(JNIEnv *env, jobject  /* this */) {
+
+    char info[40000] = {0};
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%sdecode:", info);
+        } else {
+            sprintf(info, "%sencode:", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s(video):", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s(audio):", info);
+                break;
+            default:
+                sprintf(info, "%s(other):", info);
+                break;
+        }
+        sprintf(info, "%s[%s]\n", info, c_temp->name);
+        c_temp = c_temp->next;
+    }
+    return env->NewStringUTF(info);
+    }
+}
